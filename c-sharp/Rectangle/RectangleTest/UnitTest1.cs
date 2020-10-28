66dc170b4612bcd95f6b80b7df088b39417c590b
Chris McCauley <cdmccauley@gmail.com>
2020-10-27 21:04:38 -0600

    ditto

--- c-sharp/Rectangle/RectangleTest/UnitTest1.cs
+++ c-sharp/Rectangle/RectangleTest/UnitTest1.cs
@@ -8,51 +8,21 @@ namespace RectangleTest
         [TestMethod]
         public void TestPerimeter()
         {
-            // declarations
-            Box testBox = new Box(12, 8);
-            double expected = 40;
-            double actual;
-
-            // call method
-            actual = testBox.getPerimeter();
-
-            // test variables
-            Assert.AreEqual(expected, actual);
-
+            Assert.AreEqual(40, new Box(12, 8).getPerimeter());
             Assert.AreEqual(40, new Box(12, 8).getPerimeter());
         }
 
         [TestMethod]
         public void TestArea()
         {
-            // declarations
-            Box testBox = new Box(12, 8);
-            double expected = 96;
-            double actual;
-
-            // call method
-            actual = testBox.getArea();
-
-            // test variables
-            Assert.AreEqual(expected, actual);
-
+            Assert.AreEqual(96, new Box(12, 8).getArea());
             Assert.AreEqual(96, new Box(12, 8).getArea());
         }
 
         [TestMethod]
         public void TestVolume()
         {
-            // declarations
-            Box testBox = new Box(12, 8, 10);
-            double expected = 960;
-            double actual;
-
-            // call method
-            actual = testBox.getVolume();
-
-            // test variables
-            Assert.AreEqual(expected, actual);
-
+            Assert.AreEqual(960, new Box(12, 8, 10).getVolume());
             Assert.AreEqual(960, new Box(12, 8, 10).getVolume());
         }
     }
