0ef33c707ec735cc0a9a707bfb022e370d74d317
Chris McCauley <cdmccauley@gmail.com>
2020-10-29 18:43:39 -0600

    finished c# project 2 p2

--- c-sharp/Rectangle/RectangleTest/UnitTest1.cs
+++ c-sharp/Rectangle/RectangleTest/UnitTest1.cs
@@ -9,21 +9,28 @@ namespace RectangleTest
         public void TestPerimeter()
         {
             Assert.AreEqual(40, new Box(12, 8).getPerimeter());
-            Assert.AreEqual(40, new Box(12, 8).getPerimeter());
+            Assert.AreEqual(30, new Box(5, 10).getPerimeter());
+            Assert.AreEqual(44, new Box(12, 10).getPerimeter());
+            Assert.AreEqual(60, new Box(10, 20).getPerimeter());
+
         }
 
         [TestMethod]
         public void TestArea()
         {
             Assert.AreEqual(96, new Box(12, 8).getArea());
-            Assert.AreEqual(96, new Box(12, 8).getArea());
+            Assert.AreEqual(50, new Box(5, 10).getArea());
+            Assert.AreEqual(120, new Box(12, 10).getArea());
+            Assert.AreEqual(200, new Box(10, 20).getArea());
         }
 
         [TestMethod]
         public void TestVolume()
         {
             Assert.AreEqual(960, new Box(12, 8, 10).getVolume());
-            Assert.AreEqual(960, new Box(12, 8, 10).getVolume());
+            Assert.AreEqual(750, new Box(5, 10, 15).getVolume());
+            Assert.AreEqual(1080, new Box(12, 10, 9).getVolume());
+            Assert.AreEqual(6000, new Box(10, 20, 30).getVolume());
         }
     }
 }
