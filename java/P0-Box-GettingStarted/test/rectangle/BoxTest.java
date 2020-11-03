c3021f4f1e1003eb4e03092fc9941374dd9b2eb8
Chris McCauley <Chris.McCauley@davistech.edu>
2020-11-02 19:21:50 -0700

    finished java project 3 part 2

--- java/P0-Box-GettingStarted/test/rectangle/BoxTest.java
+++ java/P0-Box-GettingStarted/test/rectangle/BoxTest.java
@@ -11,29 +11,31 @@ public class BoxTest {
     @Test
     public void testPerimeter() {
         System.out.println("perimeter");
-        Box instance = new Box(12, 8);
-        int expResult = 40;
-        int result = instance.perimeter();
-        assertEquals(expResult, result);
         
+        assertEquals(40, new Box(12, 8).perimeter());
+        assertEquals(30, new Box(5, 10).perimeter());
+        assertEquals(44, new Box(12, 10).perimeter());
+        assertEquals(60, new Box(10, 20).perimeter());
     }
 
     @Test
     public void testArea() {
         System.out.println("area");
-        Box instance = new Box(12, 8);
-        int expResult = 96;
-        int result = instance.area();
-        assertEquals(expResult, result);
+        
+        assertEquals(96, new Box(12, 8).area());
+        assertEquals(50, new Box(5, 10).area());
+        assertEquals(120, new Box(12, 10).area());
+        assertEquals(200, new Box(10, 20).area());
     }
     
     @Test
     public void testVolume() {
         System.out.println("volume");
-        Box instance = new Box(12, 8, 10);
-        int expResult = 960;
-        int result = instance.volume();
-        assertEquals(expResult, result);
+        
+        assertEquals(960, new Box(12, 8, 10).volume());
+        assertEquals(750, new Box(5, 10, 15).volume());
+        assertEquals(1080, new Box(12, 10, 9).volume());
+        assertEquals(6000, new Box(10, 20, 30).volume());
     }
     
 }
