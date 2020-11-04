b5eddb80865a5b8d20ac8fe42942ff7deb6acdf6
Chris McCauley <cdmccauley@gmail.com>
2020-11-03 19:39:22 -0700

    need to review c# project 4

--- c-sharp/LeapYear/LeapYear/Program.cs
+++ c-sharp/LeapYear/LeapYear/Program.cs
@@ -9,14 +9,42 @@ namespace LeapYear
             Console.WriteLine("Hello World!");
         }
 
-        public static int numFebDays()
+        public static int numFebDays(int year)
         {
-            return 0;
+            int days;
+
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                    days = 29;
+                else
+                    days = 28;
+            }
+            else if (year % 4 == 0)
+                days = 29;
+            else
+                days = 28;
+
+            return days;
         }
 
-        public static bool isLeapYear()
+        public static bool isLeapYear(int year)
         {
-            return true;
+            bool theAnswer;
+
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                    theAnswer = true;
+                else
+                    theAnswer = false;
+            }
+            else if (year % 4 == 0)
+                theAnswer = true;
+            else
+                theAnswer = false;
+
+            return theAnswer;
         }
     }
 }
--- c-sharp/LeapYear/LeapYearTest/UnitTest1.cs
+++ c-sharp/LeapYear/LeapYearTest/UnitTest1.cs
@@ -8,15 +8,35 @@ namespace LeapYearTest
         [TestMethod]
         public void TestNumFebDays()
         {
-            // not able to run?
-            Assert.Equals(0, LeapYear.Program.numFebDays());
+            Assert.AreEqual(28, LeapYear.Program.numFebDays(2007));
+
+            Assert.AreEqual(29, LeapYear.Program.numFebDays(2000));
+            Assert.AreEqual(28, LeapYear.Program.numFebDays(2001));
+            Assert.AreEqual(29, LeapYear.Program.numFebDays(1904));
+            Assert.AreEqual(28, LeapYear.Program.numFebDays(1900));
+            Assert.AreEqual(29, LeapYear.Program.numFebDays(2020));
+            Assert.AreEqual(28, LeapYear.Program.numFebDays(2015));
+            Assert.AreEqual(29, LeapYear.Program.numFebDays(2012));
+            Assert.AreEqual(28, LeapYear.Program.numFebDays(1950));
+            Assert.AreEqual(29, LeapYear.Program.numFebDays(2004));
+            Assert.AreEqual(28, LeapYear.Program.numFebDays(2100));
         }
 
         [TestMethod]
         public void TestIsLeapYear()
         {
-            // not able to run?
-            Assert.IsTrue(LeapYear.Program.isLeapYear());
+            Assert.IsFalse(LeapYear.Program.isLeapYear(2007));
+
+            Assert.IsTrue(LeapYear.Program.isLeapYear(2000));
+            Assert.IsFalse(LeapYear.Program.isLeapYear(2001));
+            Assert.IsTrue(LeapYear.Program.isLeapYear(1904));
+            Assert.IsFalse(LeapYear.Program.isLeapYear(1900));
+            Assert.IsTrue(LeapYear.Program.isLeapYear(2020));
+            Assert.IsFalse(LeapYear.Program.isLeapYear(2015));
+            Assert.IsTrue(LeapYear.Program.isLeapYear(2012));
+            Assert.IsFalse(LeapYear.Program.isLeapYear(1950));
+            Assert.IsTrue(LeapYear.Program.isLeapYear(2004));
+            Assert.IsFalse(LeapYear.Program.isLeapYear(2100));
         }
     }
 }
