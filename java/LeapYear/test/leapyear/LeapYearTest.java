3f89d8d5dd7f82f8eff6b1fc98475d6469caa5e2
Chris McCauley <cdmccauley@gmail.com>
2020-11-05 19:21:32 -0700

    started java project 4

---
+++ java/LeapYear/test/leapyear/LeapYearTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leapyear;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mccaulcd
+ */
+public class LeapYearTest {
+    
+    public LeapYearTest() {
+    }
+
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        LeapYear.main(args);
+        fail("The test case is a prototype.");
+    }
+    
+}
