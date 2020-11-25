ea72a9e573938f033195ff4fc98b58bce0003b36
Chris McCauley <cdmccauley@gmail.com>
2020-11-24 19:16:31 -0700

    finished c# project 6

--- c-sharp/MPGCalculator/MPGCalculatorTest/TestMPGCalculator.cs
+++ c-sharp/MPGCalculator/MPGCalculatorTest/TestMPGCalculator.cs
@@ -9,16 +9,22 @@ namespace MPGCalculatorTest
         public void TestMPGInstance()
         {
             // arrange
-            MPG instance = new MPG(300, 10);
+            MPG instanceOne = new MPG(300, 10);
+            MPG instanceTwo = new MPG(400, 20);
 
             // act and assert
-            Assert.AreEqual(300, instance.Miles, .00);
-            Assert.AreEqual(10, instance.Gallons, .00);
-            Assert.AreEqual(30, instance.Mpg, .00);
-            Assert.AreEqual(300, MPG.TotalMiles, .00);
-            Assert.AreEqual(10, MPG.TotalGallons, .00);
-            Assert.AreEqual(30, MPG.TotalMPG, .00);
-            Assert.AreEqual(1, MPG.NumTrips);
+            Assert.AreEqual(300, instanceOne.Miles, .00);
+            Assert.AreEqual(10, instanceOne.Gallons, .00);
+            Assert.AreEqual(30, instanceOne.Mpg, .00);
+
+            Assert.AreEqual(400, instanceTwo.Miles, .00);
+            Assert.AreEqual(20, instanceTwo.Gallons, .00);
+            Assert.AreEqual(20, instanceTwo.Mpg, .00);
+
+            Assert.AreEqual(700, MPG.TotalMiles, .00);
+            Assert.AreEqual(30, MPG.TotalGallons, .00);
+            Assert.AreEqual(23.33, MPG.TotalMPG, .01);
+            Assert.AreEqual(2, MPG.NumTrips);
         }
     }
 }
