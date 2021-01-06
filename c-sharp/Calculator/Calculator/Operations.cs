1ef1052fef6e0562c2c5ad0e68181777b998b24d
Chris McCauley <cdmccauley@gmail.com>
2021-01-05 20:43:15 -0700

    finished c# mod 3 comp, want to review int division test

--- c-sharp/Calculator/Calculator/Operations.cs
+++ c-sharp/Calculator/Calculator/Operations.cs
@@ -11,8 +11,18 @@ public class Operations
 		return left + right;
     }
 
+	public int Addition(int left, int right)
+	{
+		return left + right;
+	}
+
 	public double Subtraction(double left, double right)
     {
+		return left - right;
+	}
+
+	public int Subtraction(int left, int right)
+	{
 		return left - right;
 	}
 
@@ -21,8 +31,18 @@ public class Operations
 		return left * right;
 	}
 
+	public int Multiplication(int left, int right)
+	{
+		return left * right;
+	}
+
 	public double Division(double left, double right)
 	{
 		return right != 0 ? left / right : double.NaN;
+	}
+
+	public int Division(int left, int right)
+	{
+		return right != 0 ? left / right : 0;
 	}
 }
--- c-sharp/Calculator/CalculatorTests/TestCalculator.cs
+++ c-sharp/Calculator/CalculatorTests/TestCalculator.cs
@@ -20,12 +20,48 @@ namespace CalculatorTests
             Assert.AreEqual(17.0, testInstance.Addition(12.0, 5.0));
         }
 
+        [DataRow(0.0, 0.0, 0.0)]
+        [DataRow(15.0, 10.0, 5.0)]
+        [DataRow(17.0, 12.0, 5.0)]
+        [DataTestMethod]
+        public void TestDoubleAddition(double expected, double left, double right)
+        {
+            Assert.AreEqual(expected, testInstance.Addition(left, right));
+        }
+
+        [DataRow(0, 0, 0)]
+        [DataRow(15, 10, 5)]
+        [DataRow(17, 12, 5)]
+        [DataTestMethod]
+        public void TestIntegerAddition(int expected, int left, int right)
+        {
+            Assert.AreEqual(expected, testInstance.Addition(left, right));
+        }
+
         [TestMethod]
         public void TestSubtraction()
         {
             Assert.AreEqual(-0.0, testInstance.Subtraction(0.0, 0.0));
             Assert.AreEqual(5.0, testInstance.Subtraction(10.0, 5.0));
             Assert.AreEqual(7.0, testInstance.Subtraction(12.0, 5.0));
+        }
+
+        [DataRow(-0.0, 0.0, 0.0)]
+        [DataRow(5.0, 10.0, 5.0)]
+        [DataRow(7.0, 12.0, 5.0)]
+        [DataTestMethod]
+        public void TestDoubleSubtraction(double expected, double left, double right)
+        {
+            Assert.AreEqual(expected, testInstance.Subtraction(left, right));
+        }
+
+        [DataRow(-0, 0, 0)]
+        [DataRow(5, 10, 5)]
+        [DataRow(7, 12, 5)]
+        [DataTestMethod]
+        public void TestIntegerSubtraction(int expected, int left, int right)
+        {
+            Assert.AreEqual(expected, testInstance.Subtraction(left, right));
         }
 
         [TestMethod]
@@ -36,12 +72,48 @@ namespace CalculatorTests
             Assert.AreEqual(60.0, testInstance.Multiplication(12.0, 5.0));
         }
 
+        [DataRow(0.0, 0.0, 0.0)]
+        [DataRow(100.0, 10.0, 10.0)]
+        [DataRow(60.0, 12.0, 5.0)]
+        [DataTestMethod]
+        public void TestDoubleMultiplication(double expected, double left, double right)
+        {
+            Assert.AreEqual(expected, testInstance.Multiplication(left, right));
+        }
+
+        [DataRow(0, 0, 0)]
+        [DataRow(100, 10, 10)]
+        [DataRow(60, 12, 5)]
+        [DataTestMethod]
+        public void TestIntegerMultiplication(int expected, int left, int right)
+        {
+            Assert.AreEqual(expected, testInstance.Multiplication(left, right));
+        }
+
         [TestMethod]
         public void TestDivision()
         {
             Assert.AreEqual(double.NaN, testInstance.Division(0.0, 0.0));
             Assert.AreEqual(0.0, testInstance.Division(0.0, 80.0));
             Assert.AreEqual(2.4, testInstance.Division(12.0, 5.0));
+        }
+
+        [DataRow(double.NaN, 0.0, 0.0)]
+        [DataRow(0.0, 0.0, 80.0)]
+        [DataRow(2.4, 12.0, 5.0)]
+        [DataTestMethod]
+        public void TestDoubleDivision(double expected, double left, double right)
+        {
+            Assert.AreEqual(expected, testInstance.Division(left, right));
+        }
+
+        [DataRow(0, 0, 0)]
+        [DataRow(0, 0, 80)]
+        [DataRow(2, 12, 5)]
+        [DataTestMethod]
+        public void TestIntegerDivision(int expected, int left, int right)
+        {
+            Assert.AreEqual(expected, testInstance.Division(left, right));
         }
     }
 }
